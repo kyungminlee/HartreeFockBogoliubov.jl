8987a5f44a15e288dec121165b787c30ac1f835d
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2017-05-27 17:31:17 -0400

    updated make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,17 @@
 using Documenter
 using HartreeFockBogoliubov
 
-makedocs()
+makedocs(
+    authors="Kyungmin Lee",
+    sitename="HartreeFockBogoliubov.jl",
+    pages = Any[
+        "HOME" => "index.md",
+        "Basics" => Any[
+            "basics/hartreefockbogoliubov.md",
+            "basics/momentumspace.md"
+        ]
+    ]
+)
 
 deploydocs(
   deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
