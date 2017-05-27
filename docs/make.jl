aa370022ad1452b8c7812c0ac6de083bc35148e7
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2017-05-27 17:24:07 -0400

    fixed make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,10 @@
-push!(LOAD_PATH, "../src")
-
 using Documenter
 using HartreeFockBogoliubov
 
-
-makedocs(
-)
-
+makedocs()
 
 deploydocs(
   deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
-  repo = "https://github.com/kyungminlee/HartreeFockBogoliubov.jl.git",
+  repo = "github.com/kyungminlee/HartreeFockBogoliubov.jl.git",
   julia = "0.5",
 )
