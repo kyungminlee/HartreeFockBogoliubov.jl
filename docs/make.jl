5be06d35eadaa9afbfb9628503c8076995200e84
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2017-05-28 00:35:24 -0400

    removed parameters from makedocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,7 @@
 using Documenter
 using HartreeFockBogoliubov
 
-makedocs(
-    authors="Kyungmin Lee",
-    sitename="HartreeFockBogoliubov.jl",
-    pages = Any[
-        "HOME" => "index.md",
-        "Basics" => Any[
-            "basics/hartreefockbogoliubov.md",
-            "basics/momentumspace.md"
-        ],
-        "Internals" => Any[
-          "internals.md",
-          "internals/periodicsystem.md"
-        ]
-    ]
-)
+makedocs()
 
 deploydocs(
   deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
