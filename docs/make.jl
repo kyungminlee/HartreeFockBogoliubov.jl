c56d5decec1344a933f0d68ea44aa13ff0046a73
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2018-02-02 21:31:53 -0500

    updated documentation theme

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ using HartreeFockBogoliubov
 makedocs()
 
 deploydocs(
-  deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
+  deps = Deps.pip("pygments", "mkdocs", "python-markdown-math", "mkdocs-cinder"),
   repo = "github.com/kyungminlee/HartreeFockBogoliubov.jl.git",
   julia = "0.6",
 )
