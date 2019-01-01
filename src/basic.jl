f33bf91959bac3e286fcb3f4a5f773fdb9557306
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-01-01 18:41:52 -0500

    Update basic.jl to use throw ArgumentError

--- src/basic.jl
+++ src/basic.jl
@@ -9,23 +9,27 @@ For energy whose absolute value is less than `etol`, return 0.5.
 function fermidirac(temperature ::T;
                     ttol::T=eps(T),
                     etol::T=sqrt(eps(T))) where {T <:AbstractFloat}
-    @assert(temperature >= 0)
-    @assert(ttol >= 0)
-    @assert(etol >= 0)
+    if ttol < 0
+        throw(ArgumentError("ttol should be non-negative"))
+    elseif etol < 0
+        throw(ArgumentError("etol should be non-negative"))
+    elseif temperature < 0
+        throw(ArgumentError("temperature should be non-negative"))
+    end
     if temperature < ttol
         function(e ::T)
             if e <= -etol
-                return T(1.0)
+                return T(1)
             elseif e < etol
                 return T(0.5)
             else
-                return T(0.0)
+                return T(0)
             end
         end
     else
         beta = 1.0 / temperature
         function(e ::T)
-            return T(1.0 / (exp(beta * e) + 1.0))
+            return T(1 / (exp(beta * e) + 1))
         end
     end
 end
@@ -38,11 +42,15 @@ Return the Fermi-Dirac distribution function for the given temperature.
 For energy whose absolute value is less than `etol`, return 0.5.
 """
 function fermidirac(temperature ::T;
-    ttol::Float64=eps(Float64),
-    etol::Float64=sqrt(eps(Float64))) where {T <:Integer}
-    @assert(temperature >= 0, "temperature should be non-negative")
-    @assert(ttol >= 0, "ttol should be non-negative")
-    @assert(etol >= 0, "etol should be non-negative")
+                    ttol::Float64=eps(Float64),
+                    etol::Float64=sqrt(eps(Float64))) where {T <:Integer}
+    if ttol < 0
+        throw(ArgumentError("ttol should be non-negative"))
+    elseif etol < 0
+        throw(ArgumentError("etol should be non-negative"))
+    elseif temperature < 0
+        throw(ArgumentError("temperature should be non-negative"))
+    end
     if temperature == 0
         function(e ::Float64)
             if e <= -etol
@@ -54,9 +62,9 @@ function fermidirac(temperature ::T;
             end
         end
     else
-        beta = 1.0 / Float64(temperature)
+        beta = 1 / Float64(temperature)
         function(e ::Float64)
-            return 1.0 / (exp(beta * e) + 1.0)
+            return 1 / (exp(beta * e) + 1)
         end
     end
 end
