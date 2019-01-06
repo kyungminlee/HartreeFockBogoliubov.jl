46e8fc2270aa0bbd623dce1e8c09a2eda01d4eb0
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-01-06 16:33:27 -0500

    Remove osname option from deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -6,5 +6,4 @@ makedocs(sitename="HartreeFockBogoliubov.jl")
 deploydocs(
   deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
   repo = "github.com/kyungminlee/HartreeFockBogoliubov.jl.git",
-  osname = "linux",
 )
