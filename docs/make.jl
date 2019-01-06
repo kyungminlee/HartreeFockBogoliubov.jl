104d504004afb69dc6da837846917a186ca69572
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-01-06 16:16:47 -0500

    Remove option julia from deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -6,6 +6,5 @@ makedocs(sitename="HartreeFockBogoliubov.jl")
 deploydocs(
   deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
   repo = "github.com/kyungminlee/HartreeFockBogoliubov.jl.git",
-  julia = "1.0",
   osname = "linux",
 )
