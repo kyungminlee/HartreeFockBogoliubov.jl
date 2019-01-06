3733e638f35c5e982ac3054024a8cfae613f4567
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2019-01-06 18:20:28 -0500

    Update documentation setting

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,33 @@
 using Documenter
 using HartreeFockBogoliubov
 
-makedocs(sitename="HartreeFockBogoliubov.jl")
+makedocs(
+	modules=[HartreeFockBogoliubov],
+	doctest=false,
+	clean=true,
+	checkdocs=:all,
+	format=Documenter.HTML(),
+	authors="Kyungmin Lee",
+	sitename="HartreeFockBogoliubov.jl",
+	pages=[
+			"Home" => "index.md",
+			"Basic Theory" => [
+				"Hartree-Fock-Bogoliubov Theory" => "basics/hartreefockbogoliubov.md",
+				"Hartree-Fock-Bogoliubov Equations" => "basics/hfbequation.md",
+				"Hartree-Fock-Bogoliubov Decomposition" => "basics/hfbdecomposition.md",
+				"Momentum Space Formulation" => "basics/momentumspace.md",
+				"Topological Invariants" => "basics/topology.md",
+			],
+			"User Guide" => [
+				"Lattice" => "guide/lattice.md",
+				"Hamiltonian" => "guide/hamiltonian.md",
+				"HFB" => "guide/hfb.md",
+				"Linearized Gap Equation" => "guide/linearizedgap.md",
+				"Topology" => "guide/topology.md",
+				"Example" => "guide/example.md",
+			],
+		]
+	)
 
 deploydocs(
   deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
